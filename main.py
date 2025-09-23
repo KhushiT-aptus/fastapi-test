69f347434feee3552b90819d1584a5da09fb6e1e
KhushiT-aptus <khushi.thacker@aptusdatalabs.com>
2025-09-23 20:21:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ async def read_root():
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
-    print("test12")
+    print("test13")
     return {"item_id": item_id, "q": q}
